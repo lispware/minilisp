a37abb3c7ee87f2ff3630d5cdb95b618543b7d57
CK Kashyap <ckkashyap@gmail.com>
2021-02-25 21:30:26 -0800

    all good

--- src/def32.d
+++ src/def32.d
@@ -9,67 +9,67 @@ extern any Mem[];
 #define Zero (any)(Mem+18)
 #define One (any)(Mem+24)
 #define doBye_D (any)(Mem+30)
-any doBye(any);
+any doBye(Context *, any);
 #define doDe_D (any)(Mem+33)
-any doDe(any);
+any doDe(Context *, any);
 #define doAdd_D (any)(Mem+36)
-any doAdd(any);
+any doAdd(Context *, any);
 #define doSub_D (any)(Mem+39)
-any doSub(any);
+any doSub(Context *, any);
 #define doMul_D (any)(Mem+42)
-any doMul(any);
+any doMul(Context *, any);
 #define doLet_D (any)(Mem+45)
-any doLet(any);
+any doLet(Context *, any);
 #define doPrin_D (any)(Mem+48)
-any doPrin(any);
+any doPrin(Context *, any);
 #define doDo_D (any)(Mem+57)
-any doDo(any);
+any doDo(Context *, any);
 #define doSetq_D (any)(Mem+60)
-any doSetq(any);
+any doSetq(Context *, any);
 #define doMake_D (any)(Mem+63)
-any doMake(any);
+any doMake(Context *, any);
 #define doLink_D (any)(Mem+66)
-any doLink(any);
+any doLink(Context *, any);
 #define doCons_D (any)(Mem+69)
-any doCons(any);
+any doCons(Context *, any);
 #define doCar_D (any)(Mem+72)
-any doCar(any);
+any doCar(Context *, any);
 #define doCdr_D (any)(Mem+75)
-any doCdr(any);
+any doCdr(Context *, any);
 #define doQuote_D (any)(Mem+78)
-any doQuote(any);
+any doQuote(Context *, any);
 #define doDump_D (any)(Mem+87)
-any doDump(any);
+any doDump(Context *, any);
 #define doWhile_D (any)(Mem+90)
-any doWhile(any);
+any doWhile(Context *, any);
 #define doIn_D (any)(Mem+99)
-any doIn(any);
+any doIn(Context *, any);
 #define doOut_D (any)(Mem+102)
-any doOut(any);
+any doOut(Context *, any);
 #define doChar_D (any)(Mem+105)
-any doChar(any);
+any doChar(Context *, any);
 #define doLine_D (any)(Mem+108)
-any doLine(any);
+any doLine(Context *, any);
 #define doNot_D (any)(Mem+111)
-any doNot(any);
+any doNot(Context *, any);
 #define doFor_D (any)(Mem+114)
-any doFor(any);
+any doFor(Context *, any);
 #define doRun_D (any)(Mem+117)
-any doRun(any);
+any doRun(Context *, any);
 #define doHS_D (any)(Mem+120)
-any doHS(any);
+any doHS(Context *, any);
 #define doEq_D (any)(Mem+123)
-any doEq(any);
+any doEq(Context *, any);
 #define doIf_D (any)(Mem+126)
-any doIf(any);
+any doIf(Context *, any);
 #define doLongFunc_D (any)(Mem+183)
-any doLongFunc(any);
+any doLongFunc(Context *, any);
 #define doVeryLongFunc_D (any)(Mem+195)
-any doVeryLongFunc(any);
+any doVeryLongFunc(Context *, any);
 #define doCall_D (any)(Mem+216)
-any doCall(any);
+any doCall(Context *, any);
 #define doPack_D (any)(Mem+219)
-any doPack(any);
+any doPack(Context *, any);
 
 #define MEMS 222
 
