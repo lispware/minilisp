87508623446d0b62447ce672192f16a520d95b97
CK Kashyap <ckkashyap@gmail.com>
2021-02-28 12:07:36 -0800

    fix build

---
+++ src/platform/platform.h
@@ -0,0 +1,10 @@
+#ifndef __PLATFORM_H__
+#define __PLATFORM_H__
+
+
+typedef void * (*thread_func_t)(void *);
+
+
+void thread_start(Context *CONTEXT_PTR, thread_func_t FUNC, int wait);
+
+#endif
---
+++ src/platform/windows/thread.c
@@ -0,0 +1,8 @@
+#include "../platform.h"
+
+
+
+void thread_start(Context *CONTEXT_PTR, thread_func_t FUNC, int wait)
+{
+    FUNC();
+}
