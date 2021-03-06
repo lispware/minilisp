95d704830f86f8d9c51870aa8a104ea98072cd96
CK Kashyap <ckkashyap@gmail.com>
2021-03-06 07:42:47 -0800

    missedfile

---
+++ src/platform.c
@@ -0,0 +1,27 @@
+#include "lisp.h"
+#include "cell.h"
+#include "platform/platform.h"
+
+#if INTPTR_MAX == INT32_MAX
+    #include "def32.d"
+#elif INTPTR_MAX == INT64_MAX
+    #include "def64.d"
+#else
+    #error "Unsupported bit width"
+#endif
+
+
+any doSleep(Context *CONTEXT_PTR, any ex)
+{
+    uword n;
+    any x,y;
+    x = cdr(ex);
+    if (isNil(y = EVAL(CONTEXT_PTR, car(x))))
+        return Nil;
+    NeedNum(ex,y);
+    n = unBox(y);
+
+    plt_sleep(n);
+
+    return x;
+}
