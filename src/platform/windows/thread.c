798a170f8521c2465b46e2d5dda2b444e5a0193a
CK Kashyap <ckkashyap@gmail.com>
2021-03-17 19:23:49 -0700

    thread for windows

--- src/platform/windows/thread.c
+++ src/platform/windows/thread.c
@@ -3,5 +3,10 @@
 
 void plt_thread_start(Context *CONTEXT_PTR, thread_func_t FUNC, int wait)
 {
-        FUNC(CONTEXT_PTR);
+    // TODO - wait is not supported yet
+    if (wait)
+    {
+        printf("Waiting for the completion of a thread is not supported yet\n");
+    }
+    _beginthread(FUNC, 0, CONTEXT_PTR);
 }
