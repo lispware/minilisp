f4935fd1bcba6784be73a5ed9c5dfede6cfbd3bd
CK Kashyap <ckkashyap@gmail.com>
2021-04-20 10:19:50 -0700

    test

--- src/platform/windows/listen.c
+++ src/platform/windows/listen.c
@@ -1,6 +1,15 @@
 #include "../../lisp.h"
 #include "../platform.h"
 
+any plt_bind(Context *CONTEXT_PTR, word n)
+{
+    cell c1;
+
+    Push(c1, mkNum(CONTEXT_PTR, n));
+
+    return Pop(c1);
+}
+
 any plt_listen(Context *CONTEXT_PTR, word n)
 {
     cell c1;
