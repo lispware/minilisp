fda393115c94dc49e3bfad101937fcdb977d3c98
CK Kashyap <ckkashyap@gmail.com>
2021-11-25 16:11:58 -0800

    step

--- src/lisp/sym/isIntern.c
+++ src/lisp/sym/isIntern.c
@@ -5,32 +5,18 @@ any isIntern(Context *CONTEXT_PTR, any nm, any tree[2])
     any x, y, z;
     word n;
 
-    if (isTxt(nm))
+    for (x = tree[1];  x != Nil;)
     {
-        for (x = tree[0];  x != Nil;)
+        y = nm->car;
+        z = x->car->car;
+        while ((n = (word)(car(y)) - (word)car(z)) == 0)
         {
-            if ((n = (word)(car(nm)) - (word)caar(x)) == 0)
-            {
-                return car(x);
-            }
-            x = n<0? cadr(x) : cddr(x);
+            if (getCARType(y) != BIN) return car(x);
+            y=y->cdr;
+            z=z->cdr;
         }
-    }
-    else
-    {
-        for (x = tree[1];  x != Nil;)
-        {
-            y = nm->car;
-            z = x->car->car;
-            while ((n = (word)(car(y)) - (word)car(z)) == 0)
-            {
-                if (getCARType(y) != BIN) return car(x);
-                y=y->cdr;
-                z=z->cdr;
-            }
 
-            x = n<0? cadr(x) : cddr(x);
-        }
+        x = n<0? cadr(x) : cddr(x);
     }
 
 
