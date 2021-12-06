bd76cb14096d75c9b752469f5eeacb34370a7030
CK Kashyap <ckkashyap@gmail.com>
2021-12-05 16:00:12 -0800

    step

--- src/lisp/list/length.c
+++ src/lisp/list/length.c
@@ -4,7 +4,7 @@ uword length(Context *CONTEXT_PTR, any x)
 {
    uword n;
 
-   if (getCARType(x) != PTR_CELL) return 0;
+   if (!isCell(x)) return 0;
    if (cdr(x) == x) return 0;
 
    for (n = 0; x != Nil; x = cdr(x)) ++n;
