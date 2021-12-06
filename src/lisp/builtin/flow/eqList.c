f7d83709479093288eced07834050f7eb32d7498
CK Kashyap <ckkashyap@gmail.com>
2021-12-05 16:32:17 -0800

    step

--- src/lisp/builtin/flow/eqList.c
+++ src/lisp/builtin/flow/eqList.c
@@ -12,12 +12,12 @@ int eqList(Context *CONTEXT_PTR, any v1, any v2)
 
         if (t1 != t2) return -1;
 
-        if (getCARType(v1) == PTR_CELL && getCARType(car(v1)) == PTR_CELL)
+        if (isCell(v1) && isCell(car(v1)))
         {
             int r = eqList(CONTEXT_PTR, car(v1), car(v2));
             if (0 != r) return r;
         }
-        else if (getCARType(v1) != PTR_CELL)
+        else if (!isCell(v1))
         {
             int r = equal(CONTEXT_PTR, v1, v2);
             if (0 != r) return r;
