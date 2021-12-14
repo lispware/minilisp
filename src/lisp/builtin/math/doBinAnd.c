abb8621e941d597ae11ee9236c2cb72694681326
CK Kashyap <ckkashyap@gmail.com>
2021-12-13 17:43:14 -0800

    step

--- src/lisp/builtin/math/doBinAnd.c
+++ src/lisp/builtin/math/doBinAnd.c
@@ -4,8 +4,6 @@
 // (& 'num ..) -> num
 any doBinAnd(Context *CONTEXT_PTR, any ex)
 {
-    return Nil;
-#if 0
     any x, y;
     mp_err _mp_error;
 
@@ -16,22 +14,22 @@ any doBinAnd(Context *CONTEXT_PTR, any ex)
 
     mp_int *n = (mp_int*)malloc(sizeof(mp_int));
     _mp_error = mp_init(n);
-    _mp_error = mp_copy((mp_int*)y->car, n);
+    _mp_error = mp_copy(num(y), n);
 
     while (Nil != (x = cdr(x)))
     {
         if (isNil(y = EVAL(CONTEXT_PTR, car(x))))
             return Nil;
         NeedNum(ex,y);
-        mp_int *m = (mp_int*)y->car;
+        mp_int *m = num(y);
         _mp_error = mp_and(n, m, n);
 
     }
 
-    any r = cons(CONTEXT_PTR, Nil, Nil);
-    r->car = (any)n;
-    r->meta.type.parts[0] = NUM;
-    return r;
-#endif
-}
+    NewExtNum(ext, n);
 
+    any r = cons(CONTEXT_PTR, Nil, Nil);
+    r->car = (any)ext;
+    r->meta.type.parts[0] = EXT;
+    return r;
+}
