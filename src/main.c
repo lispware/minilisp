10da079d9912acb49fbb5e4621c642b34259ca95
CK Kashyap <ckkashyap@gmail.com>
2022-03-03 18:26:17 -0800

    Extracted main into a separate file

---
+++ src/main.c
@@ -0,0 +1,23 @@
+#include "lisp.h"
+
+Context LISP_CONTEXT;
+
+int main(int argc, char *av[])
+{
+    Context *CONTEXT_PTR = &LISP_CONTEXT;
+    setupBuiltinFunctions(&CONTEXT_PTR->Mem);
+    initialize_context(CONTEXT_PTR);
+    av++;
+    CONTEXT_PTR->AV = av;
+
+    CONTEXT_PTR->InFile = stdin, CONTEXT_PTR->Env.get = getStdin;
+    CONTEXT_PTR->OutFile = stdout, CONTEXT_PTR->Env.put = putStdout;
+    CONTEXT_PTR->ApplyArgs = Nil;
+    CONTEXT_PTR->ApplyBody = Nil;
+
+    loadAll(CONTEXT_PTR, NULL);
+    while (!feof(stdin))
+        load(CONTEXT_PTR, NULL, ':', Nil);
+
+    return 0;
+}
