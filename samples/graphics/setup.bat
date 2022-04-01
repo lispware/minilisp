ba35e6bcb3d3df6e2b3f3b0db375e3c0c1524b2b
CK Kashyap <ckkashyap@gmail.com>
2022-03-31 21:22:41 -0700

    win

---
+++ samples/graphics/setup.bat
@@ -0,0 +1,1 @@
+powershell -executionpolicy bypass -file setup.ps1
---
+++ samples/graphics/setup.ps1
@@ -0,0 +1,1 @@
+Invoke-WebRequest -Uri "https://www.libsdl.org/release/SDL2-devel-2.0.20-VC.zip" -OutFile "SDL2-devel-2.0.20-VC.zip"
