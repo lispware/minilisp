ce3dc7f9185d6c2bbcf3ca36f345c12cc997293f
CK Kashyap <ckkashyap@gmail.com>
2022-09-03 15:03:05 -0700

    doMul

--- src/lisp.c
+++ src/lisp.c
@@ -1915,23 +1915,22 @@ any doMul(Context *CONTEXT_PTR, any ex)
 {
     any x;
     cell c1, c2;
-    mp_err _mp_error;
 
     x = cdr(ex);
     if (isNil(data(c1) = EVAL(CONTEXT_PTR, car(x))))
     {
-        mp_int *id = (mp_int*)malloc(sizeof(mp_int));
-        _mp_error = mp_init(id); // TODO handle the errors appropriately
-        mp_set_i32(id, 1);
+        MP_INT *id = (MP_INT*)malloc(sizeof(MP_INT));
+        mpz_init(id);
+        mpz_set_ui(id, 1);
         NewNumber( id, idr);
         return idr;
     }
 
     NeedNum(ex, data(c1));
 
-    mp_int *n = (mp_int*)malloc(sizeof(mp_int));
-    _mp_error = mp_init(n); // TODO handle the errors appropriately
-    _mp_error = mp_copy(num(data(c1)), n);
+    MP_INT *n = (MP_INT*)malloc(sizeof(MP_INT));
+    mpz_init(n);
+    mpz_set(n, num(data(c1)));
 
     NewNumber( n, r);
     Push(c1, r);
@@ -1946,8 +1945,8 @@ any doMul(Context *CONTEXT_PTR, any ex)
         }
 
         NeedNum(ex,data(c2));
-        mp_int *m = num(data(c2));
-        _mp_error = mp_mul(n, m, n);
+        MP_INT *m = num(data(c2));
+        mpz_mul(n, m, n);
 
         drop(c2);
     }
