a49fd0f47af51801956d5c04bd6331ad139405df
CK Kashyap <ckkashyap@gmail.com>
2022-09-03 13:56:14 -0700

    sub

--- src/lisp.c
+++ src/lisp.c
@@ -1828,14 +1828,13 @@ any doSub(Context *CONTEXT_PTR, any ex)
 {
     any x;
     cell c1, c2;
-    mp_err _mp_error;
 
     x = cdr(ex);
     if (isNil(data(c1) = EVAL(CONTEXT_PTR, car(x))))
     {
-        mp_int *id = (mp_int*)malloc(sizeof(mp_int));
-        _mp_error = mp_init(id); // TODO handle the errors appropriately
-        mp_set_i32(id, 0);
+        MP_INT *id = (MP_INT*)malloc(sizeof(MP_INT));
+        mpz_init(id);
+        mpz_set_ui(id, 0);
 
         NewNumber( id, idr);
         return idr;
@@ -1843,9 +1842,9 @@ any doSub(Context *CONTEXT_PTR, any ex)
 
     NeedNum(ex, data(c1));
 
-    mp_int *n = (mp_int*)malloc(sizeof(mp_int));
-    _mp_error = mp_init(n); // TODO handle the errors appropriately
-    _mp_error = mp_copy(num(data(c1)), n);
+    MP_INT *n = (MP_INT*)malloc(sizeof(MP_INT));
+    mpz_init(n);
+    mpz_set(n, num(data(c1)));
 
     NewNumber( n, r);
     Push(c1, r);
@@ -1860,8 +1859,8 @@ any doSub(Context *CONTEXT_PTR, any ex)
         }
 
         NeedNum(ex,data(c2));
-        mp_int *m = num(data(c2));
-        _mp_error = mp_sub(n, m, n);
+        MP_INT *m = num(data(c2));
+        mpz_sub(n, n, m);
 
         drop(c2);
     }
