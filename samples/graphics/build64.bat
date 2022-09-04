013a7f26cb93148bf954b818ce893c8f440f9ec1
CK Kashyap <ckkashyap@gmail.com>
2022-09-04 14:09:57 -0700

    build

--- samples/graphics/build64.bat
+++ samples/graphics/build64.bat
@@ -1,4 +1,1 @@
-pushd .
-cd ../../src/libtommath && nmake -f makefile.msvc
-popd
-cl /Zi -I WindowsSDL2/SDL2-2.0.20/include -I ../../src -I ../../src/libtommath ../../src/libtommath/tommath.lib WindowsSDL2\SDL2-2.0.20\lib\x64\SDL2.lib ..\..\src\lisp.c ..\..\src\windows.c ws2_32.lib advapi32.lib  sdlmain.c /Felisp.exe
+cl -DB64 /Zi -I WindowsSDL2/SDL2-2.0.20/include -I ../../src ../../src/gmp.c WindowsSDL2\SDL2-2.0.20\lib\x64\SDL2.lib ..\..\src\lisp.c ..\..\src\windows.c ws2_32.lib advapi32.lib  sdlmain.c /Felisp.exe
