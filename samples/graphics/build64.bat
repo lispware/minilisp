3798a3f9f43c37a31cbd60a119877cbfacb86e3d
CK Kashyap <ckkashyap@gmail.com>
2022-09-04 15:32:58 -0700

    windows

--- samples/graphics/build64.bat
+++ samples/graphics/build64.bat
@@ -1,1 +1,2 @@
+rem REMEMBER to replace all long's in gmp.c and gmp.h to long long
 cl -DB64 /Zi -I WindowsSDL2/SDL2-2.0.20/include -I ../../src ../../src/gmp.c WindowsSDL2\SDL2-2.0.20\lib\x64\SDL2.lib ..\..\src\lisp.c ..\..\src\windows.c ws2_32.lib advapi32.lib  sdlmain.c /Felisp.exe
