e87838b57bd4408e758fc2b69326767a7197855d
CK Kashyap <ckkashyap@gmail.com>
2022-12-22 15:42:04 -0800

    step

--- src/lisp.c
+++ src/lisp.c
@@ -607,18 +607,10 @@ returnNULL:
 
 any mkChar(Context *CONTEXT_PTR, int c)
 {
-    cell c1;
-    any r = cons(CONTEXT_PTR, Nil, Nil);
-    Push (c1, r);
-    car(data(c1)) = cons(CONTEXT_PTR, Nil, Nil);
-    cdr(data(c1)) = Nil;
-    car(car(data(c1))) = (any)(uword)c;
-    car(cdr(data(c1))) = Nil;
-
-    setCARType(data(c1), BIN_START);
-    setCARType(car(data(c1)), BIN);
-
-    return Pop(c1);
+    unsigned char b[2];
+    b[0] = (unsigned char)c;
+    b[1] = 0;
+    return mkSym(CONTEXT_PTR, &b);
 }
 
 any mkSym(Context *CONTEXT_PTR, byte *s)
