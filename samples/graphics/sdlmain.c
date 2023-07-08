cd22d63a87a7d0cdc0af24a81d5e6f8f3cf3041a
ck <ckk@microsoft.com>
2023-07-08 10:19:06 -0700

    fixup

--- samples/graphics/sdlmain.c
+++ samples/graphics/sdlmain.c
@@ -5822,8 +5822,8 @@ int main(int argc, char* av[])
     CONTEXT_PTR->AV = av;
     CONTEXT_PTR->InFile = stdin, CONTEXT_PTR->Env.get = getStdin;
     CONTEXT_PTR->OutFile = stdout, CONTEXT_PTR->Env.put = putStdout;
-    CONTEXT_PTR->ApplyArgs = Nil;
-    CONTEXT_PTR->ApplyBody = Nil;
+    CONTEXT_PTR->ApplyArgs = cons(CONTEXT_PTR, cons(CONTEXT_PTR, consSym(CONTEXT_PTR, Nil, Nil), Nil), Nil);
+    CONTEXT_PTR->ApplyBody = cons(CONTEXT_PTR, Nil, Nil);
 
     loadAll(CONTEXT_PTR, NULL);
     while (!feof(stdin))
