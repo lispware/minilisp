51966351cbac1c5a4e540f467ade1e5230558a3d
CK Kashyap <ckkashyap@gmail.com>
2024-01-30 15:29:26 -0800

    better makefile

---
+++ src/lisp_sdl_libuv_stub.c
@@ -0,0 +1,204 @@
+#ifndef USE_LIBUV_AND_LIBSDL
+#include "pico.h"
+
+any LISP_SDL_CreateWindow(any)
+{
+	return Nil;
+}
+
+any LISP_SDL_CreateRenderer(any)
+{
+	return Nil;
+}
+
+any LISP_SDL_SetTransparency(any)
+{
+	return Nil;
+}
+
+any LISP_SDL_RenderClear(any)
+{
+	return Nil;
+}
+
+any LISP_SDL_GetWindowSurface(any)
+{
+	return Nil;
+}
+
+any LISP_IMG_Load(any)
+{
+	return Nil;
+}
+
+any LISP_SDL_CreateTextureFromSurface(any)
+{
+	return Nil;
+}
+
+any LISP_SDL_FreeSurface(any)
+{
+	return Nil;
+}
+
+any LISP_SDL_RenderCopy(any)
+{
+	return Nil;
+}
+
+any LISP_SDL_SetWindowSize(any)
+{
+	return Nil;
+}
+
+any LISP_SDL_SetWindowPosition(any)
+{
+	return Nil;
+}
+
+any LISP_SDL_PollEvent(any)
+{
+	return Nil;
+}
+
+any LISP_SDL_DestroyWindow(any)
+{
+	return Nil;
+}
+
+any LISP_SDL_Quit(any)
+{
+	return Nil;
+}
+
+any COMP_PACK(any)
+{
+	return Nil;
+}
+
+any doPACK(any)
+{
+	return Nil;
+}
+
+any LISP_SDL_Init(any)
+{
+	return Nil;
+}
+
+any LISP_SDL_RenderDrawPoint(any)
+{
+	return Nil;
+}
+
+any LISP_SDL_SetRenderDrawBlendMode(any)
+{
+	return Nil;
+}
+
+any LISP_SDL_SetTextureBlendMode(any)
+{
+	return Nil;
+}
+
+any LISP_SDL_SetTextureAlphaMod(any)
+{
+	return Nil;
+}
+
+any LISP_SDL_RenderFillRect(any)
+{
+	return Nil;
+}
+
+any LISP_SDL_RenderDrawLine(any)
+{
+	return Nil;
+}
+
+any LISP_SDL_RenderPresent(any)
+{
+	return Nil;
+}
+
+any LISP_SDL_SetRenderDrawColor(any)
+{
+	return Nil;
+}
+
+any LISP_SDL_GetMouseState(any)
+{
+	return Nil;
+}
+
+any LISP_uv_loop(any)
+{
+	return Nil;
+}
+
+any LISP_uv_run_nowait(any)
+{
+	return Nil;
+}
+
+any LISP_uv_queue_work(any)
+{
+	return Nil;
+}
+
+any LISP_fs_event_start(any)
+{
+	return Nil;
+}
+
+any LISP_fs_event_stop(any)
+{
+	return Nil;
+}
+
+any LISP_uv_tcp_connect(any)
+{
+	return Nil;
+}
+
+any LISP_uv_tcp_write(any)
+{
+	return Nil;
+}
+
+any LISP_uv_read_start(any)
+{
+	return Nil;
+}
+
+any LISP_uv_close(any)
+{
+	return Nil;
+}
+
+any LISP_uv_stop(any)
+{
+	return Nil;
+}
+
+any LISP_SDL_PushEvent(any)
+{
+	return Nil;
+}
+
+any LISP_uv_tcp_listen(any)
+{
+	return Nil;
+}
+
+any doCalloc(any)
+{
+	return Nil;
+}
+
+any doFree(any)
+{
+	return Nil;
+}
+
+#endif // USE_LIBUV_AND_LIBSDL
