071fe877ec0790a544316cd81a0075f87250a3a2
CK Kashyap <ckkashyap@gmail.com>
2024-01-30 16:08:57 -0800

    stub added correctly

--- src/lisp_sdl_libuv_stub.c
+++ src/lisp_sdl_libuv_stub.c
@@ -1,203 +1,243 @@
 #ifndef USE_LIBUV_AND_LIBSDL
 #include "pico.h"
 
-any LISP_SDL_CreateWindow(any)
+any LISP_SDL_CreateWindow(any ex)
 {
+	printf("STUB: LISP_SDL_CreateWindow\n");
 	return Nil;
 }
 
-any LISP_SDL_CreateRenderer(any)
+any LISP_SDL_CreateRenderer(any ex)
 {
+	printf("STUB: LISP_SDL_CreateRenderer\n");
 	return Nil;
 }
 
-any LISP_SDL_SetTransparency(any)
+any LISP_SDL_SetTransparency(any ex)
 {
+	printf("STUB: LISP_SDL_SetTransparency\n");
 	return Nil;
 }
 
-any LISP_SDL_RenderClear(any)
+any LISP_SDL_RenderClear(any ex)
 {
+	printf("STUB: LISP_SDL_RenderClear\n");
 	return Nil;
 }
 
-any LISP_SDL_GetWindowSurface(any)
+any LISP_SDL_GetWindowSurface(any ex)
 {
+	printf("STUB: LISP_SDL_GetWindowSurface\n");
 	return Nil;
 }
 
-any LISP_IMG_Load(any)
+any LISP_IMG_Load(any ex)
 {
+	printf("STUB: LISP_IMG_Load\n");
 	return Nil;
 }
 
-any LISP_SDL_CreateTextureFromSurface(any)
+any LISP_SDL_CreateTextureFromSurface(any ex)
 {
+	printf("STUB: LISP_SDL_CreateTextureFromSurface\n");
 	return Nil;
 }
 
-any LISP_SDL_FreeSurface(any)
+any LISP_SDL_FreeSurface(any ex)
 {
+	printf("STUB: LISP_SDL_FreeSurface\n");
 	return Nil;
 }
 
-any LISP_SDL_RenderCopy(any)
+any LISP_SDL_RenderCopy(any ex)
 {
+	printf("STUB: LISP_SDL_RenderCopy\n");
 	return Nil;
 }
 
-any LISP_SDL_SetWindowSize(any)
+any LISP_SDL_SetWindowSize(any ex)
 {
+	printf("STUB: LISP_SDL_SetWindowSize\n");
 	return Nil;
 }
 
-any LISP_SDL_SetWindowPosition(any)
+any LISP_SDL_SetWindowPosition(any ex)
 {
+	printf("STUB: LISP_SDL_SetWindowPosition\n");
 	return Nil;
 }
 
-any LISP_SDL_PollEvent(any)
+any LISP_SDL_PollEvent(any ex)
 {
+	printf("STUB: LISP_SDL_PollEvent\n");
 	return Nil;
 }
 
-any LISP_SDL_DestroyWindow(any)
+any LISP_SDL_DestroyWindow(any ex)
 {
+	printf("STUB: LISP_SDL_DestroyWindow\n");
 	return Nil;
 }
 
-any LISP_SDL_Quit(any)
+any LISP_SDL_Quit(any ex)
 {
+	printf("STUB: LISP_SDL_Quit\n");
 	return Nil;
 }
 
-any COMP_PACK(any)
+any COMP_PACK(any ex)
 {
+	printf("STUB: COMP_PACK\n");
 	return Nil;
 }
 
-any doPACK(any)
+any doPACK(any ex)
 {
+	printf("STUB: doPACK\n");
 	return Nil;
 }
 
-any LISP_SDL_Init(any)
+any LISP_SDL_Init(any ex)
 {
+	printf("STUB: LISP_SDL_Init\n");
 	return Nil;
 }
 
-any LISP_SDL_RenderDrawPoint(any)
+any LISP_SDL_RenderDrawPoint(any ex)
 {
+	printf("STUB: LISP_SDL_RenderDrawPoint\n");
 	return Nil;
 }
 
-any LISP_SDL_SetRenderDrawBlendMode(any)
+any LISP_SDL_SetRenderDrawBlendMode(any ex)
 {
+	printf("STUB: LISP_SDL_SetRenderDrawBlendMode\n");
 	return Nil;
 }
 
-any LISP_SDL_SetTextureBlendMode(any)
+any LISP_SDL_SetTextureBlendMode(any ex)
 {
+	printf("STUB: LISP_SDL_SetTextureBlendMode\n");
 	return Nil;
 }
 
-any LISP_SDL_SetTextureAlphaMod(any)
+any LISP_SDL_SetTextureAlphaMod(any ex)
 {
+	printf("STUB: LISP_SDL_SetTextureAlphaMod\n");
 	return Nil;
 }
 
-any LISP_SDL_RenderFillRect(any)
+any LISP_SDL_RenderFillRect(any ex)
 {
+	printf("STUB: LISP_SDL_RenderFillRect\n");
 	return Nil;
 }
 
-any LISP_SDL_RenderDrawLine(any)
+any LISP_SDL_RenderDrawLine(any ex)
 {
+	printf("STUB: LISP_SDL_RenderDrawLine\n");
 	return Nil;
 }
 
-any LISP_SDL_RenderPresent(any)
+any LISP_SDL_RenderPresent(any ex)
 {
+	printf("STUB: LISP_SDL_RenderPresent\n");
 	return Nil;
 }
 
-any LISP_SDL_SetRenderDrawColor(any)
+any LISP_SDL_SetRenderDrawColor(any ex)
 {
+	printf("STUB: LISP_SDL_SetRenderDrawColor\n");
 	return Nil;
 }
 
-any LISP_SDL_GetMouseState(any)
+any LISP_SDL_GetMouseState(any ex)
 {
+	printf("STUB: LISP_SDL_GetMouseState\n");
 	return Nil;
 }
 
-any LISP_uv_loop(any)
+any LISP_uv_loop(any ex)
 {
+	printf("STUB: LISP_uv_loop\n");
 	return Nil;
 }
 
-any LISP_uv_run_nowait(any)
+any LISP_uv_run_nowait(any ex)
 {
+	printf("STUB: LISP_uv_run_nowait\n");
 	return Nil;
 }
 
-any LISP_uv_queue_work(any)
+any LISP_uv_queue_work(any ex)
 {
+	printf("STUB: LISP_uv_queue_work\n");
 	return Nil;
 }
 
-any LISP_fs_event_start(any)
+any LISP_fs_event_start(any ex)
 {
+	printf("STUB: LISP_fs_event_start\n");
 	return Nil;
 }
 
-any LISP_fs_event_stop(any)
+any LISP_fs_event_stop(any ex)
 {
+	printf("STUB: LISP_fs_event_stop\n");
 	return Nil;
 }
 
-any LISP_uv_tcp_connect(any)
+any LISP_uv_tcp_connect(any ex)
 {
+	printf("STUB: LISP_uv_tcp_connect\n");
 	return Nil;
 }
 
-any LISP_uv_tcp_write(any)
+any LISP_uv_tcp_write(any ex)
 {
+	printf("STUB: LISP_uv_tcp_write\n");
 	return Nil;
 }
 
-any LISP_uv_read_start(any)
+any LISP_uv_read_start(any ex)
 {
+	printf("STUB: LISP_uv_read_start\n");
 	return Nil;
 }
 
-any LISP_uv_close(any)
+any LISP_uv_close(any ex)
 {
+	printf("STUB: LISP_uv_close\n");
 	return Nil;
 }
 
-any LISP_uv_stop(any)
+any LISP_uv_stop(any ex)
 {
+	printf("STUB: LISP_uv_stop\n");
 	return Nil;
 }
 
-any LISP_SDL_PushEvent(any)
+any LISP_SDL_PushEvent(any ex)
 {
+	printf("STUB: LISP_SDL_PushEvent\n");
 	return Nil;
 }
 
-any LISP_uv_tcp_listen(any)
+any LISP_uv_tcp_listen(any ex)
 {
+	printf("STUB: LISP_uv_tcp_listen\n");
 	return Nil;
 }
 
-any doCalloc(any)
+any doCalloc(any ex)
 {
+	printf("STUB: doCalloc\n");
 	return Nil;
 }
 
-any doFree(any)
+any doFree(any ex)
 {
+	printf("STUB: doFree\n");
 	return Nil;
 }
 
