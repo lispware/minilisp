66576d225854c8466d71892b5e2bf588b99bf7b6
CK Kashyap <ckkashyap@gmail.com>
2024-02-19 13:55:21 -0800

    mul support added

--- src/math.c
+++ src/math.c
@@ -289,15 +289,15 @@ any doMul(any ex) {
    x = cdr(ex);
    if (isNil(y = EVAL(car(x))))
       return Nil;
-   NeedNum(ex,y);
-   n = unBox(y);
+   NeedNumBF(ex,y);
+   n = unBoxBF(y);
    while (isCell(x = cdr(x))) {
       if (isNil(y = EVAL(car(x))))
          return Nil;
-      NeedNum(ex,y);
-      n *= unBox(y);
+      NeedNumBF(ex,y);
+      bf_mul((bf_t*)n,(bf_t*)n,(bf_t*)(unBoxBF(y)), LIBBF_PREC, BF_RNDN);
    }
-   return box(n);
+   return boxBF(n);
 }
 
 // (*/ 'num1 ['num2 ..] 'num3) -> num
